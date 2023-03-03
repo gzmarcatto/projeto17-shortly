031ad57fe7be2b2c4f39e76f5d957140d39d3cdb
Giovanni Marcatto <gzmarcatto@gmail.com>
2023-03-03 12:19:23 -0300

    fix: get ranking query

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -21,14 +21,14 @@ export async function getUserInfo(req, res) {
   try {
     const userId = res.locals.user.rows[0].id;
     const existsURL = await db.query(`SELECT * FROM urls WHERE "userId" = $1;`, [userId]);
-    
+
     if (existsURL.rows[0] !== undefined) {
       const result = await db.query(
         `SELECT users.id AS "id", users.name AS "name", SUM("viewCount") as "visitCount", ARRAY(SELECT JSON_BUILD_OBJECT('id', urls.id, 'shortUrl', urls."shortUrl", 'url', urls.url, 'visitCount', urls."viewCount")
             FROM urls JOIN users ON urls."userId" = users.id WHERE "userId" = $1) AS "shortenedUrls" FROM urls JOIN users ON urls."userId" = users.id WHERE "userId" = $2 GROUP BY users.id;`,
         [userId, userId]
       );
-      console.log(result)
+      console.log(result);
       return res.status(200).send(result.rows[0]);
     } else {
       const userInfo = await db.query(`SELECT * FROM users WHERE id = $1;`, [userId]);
@@ -47,11 +47,18 @@ export async function getUserInfo(req, res) {
 
 export async function getRanking(req, res) {
   try {
-    await db.query(
-      `SELECT users.id, users.name, COUNT("userId") AS "linksCount", COALESCE(SUM("viewCount"), 0) AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC NULLS LAST;`
-    );
+    await db.query(`
+    SELECT USERS.NAME,
+    COUNT("shortUrl") AS "linksCount",
+    SUM("viewCount") AS "viewCount"
+    FROM URLS
+    JOIN USERS ON URLS."userId" = USERS.ID
+    GROUP BY USERS.ID
+    ORDER BY "viewCount" DESC
+    LIMIT 10
+    `);
     return res.status(200).send(result.rows.slice(0, 10));
-  } catch (error) {
+  }catch (error) {
     return res.status(500).send(error.message);
   }
 }
